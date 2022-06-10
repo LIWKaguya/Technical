55a27ea7eabf9afc394ec707ff2a6f993dbf5216
Le Hung <hungle120901@gmail.com>
2022-06-11 05:42:28 +0700

    We getting somewhere

--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -1,31 +1,112 @@
-import { Typography, Card, CardContent, CardActions, Button, Avatar, CardHeader } from "@mui/material"
+import { Typography, Card, CardContent, CardActions, Button, Avatar, CardHeader, Grid } from "@mui/material"
 import { red } from "@mui/material/colors"
 
 const UsersTable = ({ users }) => {
     return (
-        <Card>
-            <CardHeader 
-            avatar={
-                <Avatar sx={{ bgcolor: red[500]}}>
-                    P
-                </Avatar>
-            }
-            />
-            <CardContent>
-                <Typography>
-                    {users[0]?.id}
-                </Typography>
-                <Typography>
-                    {users[0]?.name}
-                </Typography>
-                <Typography>
-                    {users[0]?.email}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">More Details</Button>
-            </CardActions>
-        </Card>
+        <>
+            <Grid container spacing={3}>
+                <Grid item xs={4}>
+                <Card>
+                    <CardHeader 
+                    avatar={
+                        <Avatar sx={{ bgcolor: red[500]}}>
+                            P
+                        </Avatar>
+                    }
+                    />
+                    <CardContent>
+                        <Typography>
+                            {users[0]?.id}
+                        </Typography>
+                        <Typography>
+                            {users[0]?.name}
+                        </Typography>
+                            <Typography>
+                            {users[0]?.email}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">More Details</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={4}>
+                <Card>
+                    <CardHeader 
+                    avatar={
+                        <Avatar sx={{ bgcolor: red[500]}}>
+                            P
+                        </Avatar>
+                    }
+                    />
+                    <CardContent>
+                        <Typography>
+                            {users[1]?.id}
+                        </Typography>
+                        <Typography>
+                            {users[1]?.name}
+                        </Typography>
+                            <Typography>
+                            {users[1]?.email}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">More Details</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={4}>
+                <Card>
+                    <CardHeader 
+                    avatar={
+                        <Avatar sx={{ bgcolor: red[500]}}>
+                            P
+                        </Avatar>
+                    }
+                    />
+                    <CardContent>
+                        <Typography>
+                            {users[2]?.id}
+                        </Typography>
+                        <Typography>
+                            {users[2]?.name}
+                        </Typography>
+                            <Typography>
+                            {users[2]?.email}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">More Details</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={4}>
+                <Card>
+                    <CardHeader 
+                    avatar={
+                        <Avatar sx={{ bgcolor: red[500]}}>
+                            P
+                        </Avatar>
+                    }
+                    />
+                    <CardContent>
+                        <Typography>
+                            {users[2]?.id}
+                        </Typography>
+                        <Typography>
+                            {users[2]?.name}
+                        </Typography>
+                            <Typography>
+                            {users[2]?.email}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">More Details</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            </Grid>
+        </>
     )
 }
 
