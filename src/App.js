1a63d9d9ed7cca8b33bb68b668ca9f8f4059356e
Le Hung <hungle120901@gmail.com>
2022-06-11 04:57:14 +0700

    I don't know why it's working now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
   const [num, setNum] = useState(0)
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
+      setUsers(response.data)
+    })
+  }, [])
+
+  console.log(users)
 
   return (
     <>
